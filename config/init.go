0a5d62e7afc3043eaf26caa2a6a98a6f31eb61e3
liangminghaoangus <liangminghaoangus@163.com>
2023-06-28 15:45:29 +0800

    add chinese font

--- config/init.go
+++ config/init.go
@@ -3,14 +3,21 @@ package config
 import (
 	"encoding/json"
 	"fmt"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
 	"os"
 	"path"
 )
 
 var config *Config
+var systemFont font.Face
 
 func GetConfig() *Config {
 	return config
+}
+
+func GetSystemFont() font.Face {
+	return systemFont
 }
 
 type Config struct {
@@ -34,4 +41,19 @@ func Init(fileName string) {
 	if err := json.Unmarshal(raw, &config); err != nil {
 		panic(err)
 	}
+
+	fontPath := path.Join(dir, "assets/font/AlibabaPuHuiTi-3-55-Regular.ttf")
+	raw, err = os.ReadFile(fontPath)
+	if err != nil {
+		panic(err)
+	}
+	tt, err := opentype.Parse(raw)
+	if err != nil {
+		panic(err)
+	}
+	systemFont, err = opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    32,
+		DPI:     72,
+		Hinting: font.HintingFull, // Use quantization to save glyph cache images.
+	})
 }
