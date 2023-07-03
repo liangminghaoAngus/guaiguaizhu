3c798a14652e835b99b762788c80da078f367b22
liangminghao <xx@qq.com>
2023-07-03 11:15:52 +0800

    add health component hp&mp ui ready for render

--- component/health.go
+++ component/health.go
@@ -1,14 +1,22 @@
 package component
 
 import (
+	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/yohamta/donburi"
+	"github.com/yohamta/donburi/features/math"
 	"liangminghaoangus/guaiguaizhu/engine"
 	"time"
 )
 
 type HealthData struct {
-	HP                   int
-	MP                   int
+	HP int
+	MP int
+
+	HPuiPos math.Vec2     // ui的位置
+	MPuiPos math.Vec2     // ui的位置
+	HPui    *ebiten.Image // 判断是否需要定制 HP 界面
+	MPui    *ebiten.Image
+
 	JustDamage           bool
 	DamageIndicatorTimer *engine.Timer
 	//DamageIndicator      *SpriteData
@@ -20,9 +28,9 @@ var Health = donburi.NewComponentType[HealthData](HealthData{
 
 func NewPlayerHealthData() HealthData {
 	return HealthData{
-		100,
-		100,
-		false,
-		engine.NewTimer(time.Millisecond * 100),
+		HP:                   100,
+		MP:                   100,
+		JustDamage:           false,
+		DamageIndicatorTimer: engine.NewTimer(time.Millisecond * 100),
 	}
 }
