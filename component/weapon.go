da5b45f80d3325ed65f87553948c990968960c63
liangminghao <xx@qq.com>
2023-07-06 17:17:04 +0800

    change render point

--- component/weapon.go
+++ component/weapon.go
@@ -30,8 +30,9 @@ func (we *WeaponHandlerData) GetRenderPoint() math.Vec2 {
 
 func (we *WeaponHandlerData) GetRenderImage() (*ebiten.Image, *ebiten.DrawImageOptions) {
 	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(-we.Width/2, -we.Height/2)
 	imgPoint := we.GetRenderPoint()
-	tranX, tranY := imgPoint.X, imgPoint.Y
+	tranX, tranY := we.Width/2+imgPoint.X, we.Height/2+imgPoint.Y
 	op.GeoM.Rotate(we.Angle)
 	op.GeoM.Translate(tranX, tranY)
 	return we.Image, op
@@ -43,9 +44,16 @@ func (we *WeaponHandlerData) GetWeaponBox() (point math.Vec2, angle float64, w, 
 	return math.Vec2{}, 0, 0, 0
 }
 
+func calculateWH(angle float64, width, length float64) (w, h float64) {
+	radians := angle * (systemMath.Pi / 180.0)
+	w = width * systemMath.Cos(radians)
+	h = length * systemMath.Sin(radians)
+	return w, h
+}
+
 func calculatePoint(start math.Vec2, angle float64, length float64) math.Vec2 {
 	radians := angle * (systemMath.Pi / 180.0)
-	x := start.X + length*systemMath.Cos(radians)
-	y := start.Y + length*systemMath.Sin(radians)
+	x := start.X - length*systemMath.Cos(radians)
+	y := start.Y - length*systemMath.Sin(radians)
 	return math.Vec2{X: x, Y: y}
 }
