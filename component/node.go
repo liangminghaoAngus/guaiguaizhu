30e09fd4b8e36c84f63927e27eaa05db33f3aa98
liangminghao <xx@qq.com>
2023-07-11 17:49:42 +0800

    todo node component

--- component/node.go
+++ component/node.go
@@ -1,8 +1,11 @@
 package component
 
 import (
+	"fmt"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/yohamta/donburi"
 	"github.com/yohamta/donburi/features/math"
+	"sort"
 )
 
 type NodeItem struct {
@@ -15,10 +18,36 @@ type NodeItem struct {
 	Angle         float64
 }
 
-type PlayerNode struct {
+type PlayerNodeData struct {
 	Head      *NodeItem
 	Body      *NodeItem
 	Hand      *NodeItem
 	LeftFoot  *NodeItem
 	RightFoot *NodeItem
+}
+
+var PlayerNode = donburi.NewComponentType[PlayerNodeData](PlayerNodeData{})
+
+func (p *PlayerNodeData) Draw() {
+	l := []*NodeItem{p.Head, p.Body, p.Head, p.LeftFoot, p.RightFoot}
+	// 删除不存在的节点
+	for i := 0; i < len(l); i++ {
+		if l[i] == nil {
+			l = append(l[:i], l[i+1:]...)
+			i--
+		}
+	}
+	sort.Slice(l, func(i, j int) bool {
+		return l[i].ZIndex < l[j].ZIndex
+	})
+	for _, node := range l {
+		fmt.Println(node)
+	}
+	// todo
+}
+
+func NewPlayerNodeData() *PlayerNodeData {
+	nodeData := &PlayerNodeData{}
+
+	return nodeData
 }
